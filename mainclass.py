9c2b133ca92f66f46f97aacd633e835cb7cf92da
teoyi <yipengteo@yahoo.com.sg>
2020-07-28 21:46:11 -0700

    Added params and user selection functionality

--- mainclass.py
+++ mainclass.py
@@ -15,7 +15,7 @@ import glob # For listing out files in a directory
 import os
 ###############################################################################
 
-class Initial():
+class Main():
     def __init__(self):
         self.lst = []
         self.params = []
@@ -26,36 +26,100 @@ class Initial():
         self.choicefile = []
         self.finalfile = []
 
-
-    def get_path(self):
+    def add_csvs(self):
         while True:
             dir = input("Paste full path to directory here: ")
             if os.path.isdir(dir) == True:
                 for files in glob.iglob(os.path.join(dir, "*.csv")):
-                    print(files)
+                    #print(files)
                     longname = os.path.basename(files)
                     self.lst.append(longname)
-                return self.lst
                 break
             else:
                 print('\n')
                 print('Sorry that is not a valid path/directory. Please try again.')
                 continue
 
-# init = Initial()
-# init.get_path()
-dir = input("Paste full path to directory here: ")
-#print(dir)
-# onlyfiles = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f))]
-# print(onlyfiles)
-if os.path.isdir(dir) == True:
-    onlyfiles = glob.iglob(os.path.join(dir,".csv"))
-    for files in onlyfiles:
-        print(files)
+    def show_csvs(self):
+        print('The following files are found in this directory: ')
+        for files in self.lst:
+            print('\t' + files)
 
-print(list(onlyfiles))
-# print(init.lst)
-print('\n')
-print('The following files are found in this directory: ')
-# for files in init.lst:
-#     print('\t' + files)
+    def check_params(self):
+        for files in self.lst:
+            split = files.split('_')
+            self.params.append(split)
+        if len(self.params) == 1:
+            self.bad_temp.append(self.params[0][0])
+            self.bad_offsetp.append(self.params[0][1])
+            self.bad_offsetn.append(self.params[0][2])
+            self.bad_IT.append(self.params[0][6])
+        else:
+            for files in range(len(self.params)-1):
+                self.bad_temp.append(self.params[files][0])
+                self.bad_offsetp.append(self.params[files][1])
+                self.bad_offsetn.append(self.params[files][2])
+                self.bad_IT.append(self.params[files][6])
+
+    def file_select(self):
+        # Extracting unique values from the list
+        exist_t = set(self.bad_temp)
+        exist_p = set(self.bad_offsetp)
+        exist_n = set(self.bad_offsetn)
+        exist_it = set(self.bad_IT)
+
+        choosing = True
+        while choosing:
+            # Narrowing down the files to what you want to use
+            while True:
+                choicetemp = input('Which temperature?: ')
+                if choicetemp in exist_t:
+                    break
+                else:
+                    print('Sorry, data with that temperature does not exist in this directory. Please try again.')
+                    continue
+            while True:
+                choiceoffsetp = input('Which (+) offset?: ')
+                if choiceoffsetp in exist_p:
+                    break
+                else:
+                    print('Sorry, data with that (+) offset does not exist. Please try again.')
+                    continue
+            while True:
+                choiceoffsetn = input('Which (-) offset?: ')
+                if choiceoffsetn in exist_n:
+                    break
+                else:
+                    print('Sorry, data with that (-) offset does not exist. Please try again.')
+                    continue
+            while True:
+                choiceIT = input('Which integration time?: ')
+                if choiceIT in exist_it:
+                    break
+                else:
+                    print('Sorry, data with that integration time does not exist. Please try again.')
+                    continue
+            break
+
+    
+init = Main()
+init.add_csvs()
+init.show_csvs()
+init.check_params()
+init.file_select()
+
+
+    # def get_path(self):
+    #     while True:
+    #         dir = input("Paste full path to directory here: ")
+    #         if os.path.isdir(dir) == True:
+    #             for files in glob.iglob(os.path.join(dir, "*.csv")):
+    #                 print(files)
+    #                 longname = os.path.basename(files)
+    #                 self.lst.append(longname)
+    #             return self.lst
+    #             break
+    #         else:
+    #             print('\n')
+    #             print('Sorry that is not a valid path/directory. Please try again.')
+    #             continue
