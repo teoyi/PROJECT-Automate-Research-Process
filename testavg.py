f65e4038becc4f7ccf282ffb87943c41735a5156
teoyi <yipengteo@yahoo.com.sg>
2020-08-01 00:26:17 -0700

    Attempted to take an average in dataframe. Failed. Waiting for help from stackoverflow.

--- testavg.py
+++ testavg.py
@@ -10,18 +10,21 @@ files_of_interests = {}
 
 for filename in files:
     if filename[-4:] == '.csv':
-        key = filename[:-4]
+        key = filename[:-5]
         files_of_interests.setdefault(key, [])
         files_of_interests[key].append(filename)
 
 print(files_of_interests)
 
-wavelength_r = np.arange(1100, 1500, 10)
+#wavelength_r = np.arange(1100, 1500, 10)
 # wl_df = pd.DataFrame.from_records([wavelength_r]).T
 # print(wl_df)
 for key in files_of_interests:
-    stack_df = pd.DataFrame.from_records([wavelength_r]).T
+    #stack_df = pd.DataFrame.from_records([wavelength_r]).T
+    #stack_df = stack_df.rename(columns = {'1', 'Wavelength'})
+    stack_df = pd.DataFrame()
     print(stack_df)
     for filename in files_of_interests[key]:
-        stack_df = stack_df.append(pd.read_csv(dir + '/' + filename, usecols = ["S2c", "S2"]))
+        stack_df = stack_df.append(pd.read_csv(os.path.join(dir, filename)))
+        #stack_df.set_index('Unnamed:0', inplace = True)
     print(stack_df)
