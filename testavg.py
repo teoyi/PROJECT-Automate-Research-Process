ea8103fa8d1588ec7e41374a9dbe219cb98cdb69
teoyi <yipengteo@yahoo.com.sg>
2020-08-01 02:30:28 -0700

    Testing suggestions from stackoverflow

--- testavg.py
+++ testavg.py
@@ -19,12 +19,18 @@ print(files_of_interests)
 #wavelength_r = np.arange(1100, 1500, 10)
 # wl_df = pd.DataFrame.from_records([wavelength_r]).T
 # print(wl_df)
+# for key in files_of_interests:
+#     #stack_df = pd.DataFrame.from_records([wavelength_r]).T
+#     #stack_df = stack_df.rename(columns = {'1', 'Wavelength'})
+#     stack_df = pd.DataFrame()
+#     print(stack_df)
+#     for filename in files_of_interests[key]:
+#         stack_df = stack_df.append(pd.read_csv(os.path.join(dir, filename)))
+#         #stack_df.set_index('Unnamed:0', inplace = True)
+    # print(stack_df)
+dflist = []
 for key in files_of_interests:
-    #stack_df = pd.DataFrame.from_records([wavelength_r]).T
-    #stack_df = stack_df.rename(columns = {'1', 'Wavelength'})
-    stack_df = pd.DataFrame()
-    print(stack_df)
     for filename in files_of_interests[key]:
-        stack_df = stack_df.append(pd.read_csv(os.path.join(dir, filename)))
-        #stack_df.set_index('Unnamed:0', inplace = True)
-    print(stack_df)
+        dflist.append(pd.read_csv(os.path.join(dir, filename)) )
+cat = pd.concat(dflist, axis = 1)
+cat.to_csv(dir + '/cat.csv')
