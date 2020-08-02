c80201064fc2f107a1f8b9456f8697e93a93aafd
teoyi <yipengteo@yahoo.com.sg>
2020-08-01 18:11:55 -0700

    Tried out second suggestion from stackoverflow

--- testavg.py
+++ testavg.py
@@ -13,9 +13,10 @@ for filename in files:
         key = filename[:-5]
         files_of_interests.setdefault(key, [])
         files_of_interests[key].append(filename)
+        print(filename)
 
 print(files_of_interests)
-
+print(os.path.join(dir, filename))
 #wavelength_r = np.arange(1100, 1500, 10)
 # wl_df = pd.DataFrame.from_records([wavelength_r]).T
 # print(wl_df)
@@ -28,9 +29,18 @@ print(files_of_interests)
 #         stack_df = stack_df.append(pd.read_csv(os.path.join(dir, filename)))
 #         #stack_df.set_index('Unnamed:0', inplace = True)
     # print(stack_df)
-dflist = []
-for key in files_of_interests:
-    for filename in files_of_interests[key]:
-        dflist.append(pd.read_csv(os.path.join(dir, filename)) )
-cat = pd.concat(dflist, axis = 1)
-cat.to_csv(dir + '/cat.csv')
+# dflist = []
+# for key in files_of_interests:
+#     for filename in files_of_interests[key]:
+#         dflist.append(pd.read_csv(os.path.join(dir, filename)) )
+# concat = pd.concat(dflist, axis = 1)
+# concat.to_csv(dir + '/concat.csv')
+# ``\
+
+#df = (pd.read_csv(os.path.join(dir, filename)) for key in files_of_interests for filename in key)
+df = pd.concat([pd.read_csv(os.path.join(dir, filename))
+                for key in files_of_interests for filename in files_of_interests[key]], axis = 0)
+
+df = df.groupby(['Unnamed: 0', 'Wavelength', 'Wavelength.1']).mean().reset_index()
+df.to_csv(dir + '/try.csv')
+print(df)
