39631a7bfcb293ac9437c7dbea43a515a88efbc8
teoyi <yipengteo@yahoo.com.sg>
2021-04-04 19:09:28 -0700

    Added file long name filtering to check for correct column format

--- redundant.py
+++ redundant.py
@@ -17,3 +17,42 @@ popt, pcov = curve_fit(fit, new_df['Photon Energy'], new_df[col[1]], maxfev = 50
 
 print(popt)
 print(pcov)
+
+#__________________________________________________________#
+
+# Applying curve fit to S2c data to determine DC offset = a
+# Solving for a to begin second correction, the DC offset 
+def ln_fit(E,a,b): 
+    return a+0.5*np.log(E-bandE)-(E/(k*b)) # Eg and k are already defined previously 
+
+E = np.linspace(np.min(new_df['Photon Energy']), np.max(new_df['Photon Energy']),1000)
+
+popt, pcov = curve_fit(ln_fit, new_df['Photon Energy'], new_df['ln(S2c)'], maxfev = 5000) # note: col[1] is S2c column 
+
+print(f"The DC offset of this data is {popt[0]}")
+dc_offset = popt[0]
+plt.plot(new_df['Photon Energy'], new_df['ln(S2c)'], 'o', label='S2c')
+plt.plot(new_df['Photon Energy'], ln_fit(new_df['Photon Energy'], *popt), '-', label='S2c fit')
+plt.ylabel('Emission Intensity (a.u.)')
+plt.xlabel('Photon Energy (eV)')
+plt.legend()
+#plt.savefig('fit.png')
+plt.show()
+
+print(dc_offset)
+
+# Applying 3rd correction, multiplier, to determine the corrected S2 values 
+new_df['Multiplier'] = new_df['S2c']/new_df['S2']
+#print(new_df)
+new_df['Corrected S2'] = (new_df['S2'] - dc_offset)*new_df['Multiplier']
+new_df['ln(Corrected S2)'] = np.log10(new_df['Corrected S2'])
+print(new_df)
+
+plt.plot(new_df['Photon Energy'], new_df['Corrected S2'],'o', label='Corrected S2')
+plt.plot(new_df['Photon Energy'], new_df['ln(Corrected S2)'],'o', label='Corrected S2')
+plt.yscale('log')
+plt.ylabel('Emission Intensity (a.u.)')
+plt.xlabel('Photon Energy (eV)')
+plt.legend()
+plt.savefig('correctedsmilog.png')
+plt.show()
--- test.py
+++ test.py
@@ -1,8 +1,118 @@
-# Loading file, and obtaining long name components 
-path = "./testfolder"
-data_list = os.listdir(path) # Create a list of all files in the folder
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import os
+import os.path
+from scipy.optimize import curve_fit
 
-current_file = data_list[2]
-print(f"The file being used is {current_file}.")
-components = current_file.split("_")
-latticeT = components[0]
+# Loading file, and obtaining long name components 
+path = "./202"
+data_list = os.listdir(path) # Create a list of all files in the folder
+#print(data_list)
+
+# Empty list will be appended with the respective inputs in the order they are listed in the folder 
+volts = [] # Empty list to store input voltage values 
+c_temp = [] # Empty list to store calculated carrier temperature values 
+
+# Constants to be used 
+h = 4.1357 * 10 ** -15 # Plancks constant in eV*s
+c = 2.99792458 * 10 ** 8 # Speed of light in m/s 
+k = 8.617333262145 * 10 ** -5 # Boltzmann constant in eV/K
+comp = 0.939162 # Composition for InGaAs, estimated by Peter, solved through Wolfram Alpha 
+
+# Creating curve fit function 
+def guess_check(title_str, xdata, ydata, label1, label2, plot_title): 
+    # Curve fit function: 
+    def first_pass(E, b, c): 
+        return c  * E * np.sqrt(E-bandE) * np.exp(-E*b) # bandE and k are already defined previously 
+
+    def second_pass(E, a, b, c): 
+        return a + c  * E * np.sqrt(E-bandE) * np.exp(-E*b) 
+
+    # Curve fit range and weight:
+    E = np.linspace(np.min(xdata), np.max(xdata), len(xdata))
+    weight_func = np.exp(E*6) #1/(np.sqrt(np.exp(-E*18)))
+
+    popt1, pcov1 = curve_fit(first_pass, xdata, ydata, maxfev = 10000, sigma = weight_func, absolute_sigma=True) 
+    #print(popt1)
+    p0_b = popt1[0]
+    p0_c = popt1[1]
+
+    popt2, pcov2 = curve_fit(second_pass, xdata, ydata, maxfev = 10000, p0 = [0,p0_b,p0_c], sigma=weight_func, absolute_sigma=True)
+    #print(popt2)
+    pfinal_a = popt2[0]
+    pfinal_b = popt2[1]
+    pfinal_c = popt2[2] 
+
+    print(f''' 
+{title_str}
+_____________________________________________
+
+Initial Guess Parameters: 
+    b = {popt1[0]}
+    c = {popt1[1]}
+
+Final Fit Parameters: 
+    a = {popt2[0]}
+    b = {popt2[1]}
+    c = {popt2[2]}
+    ''')
+
+    fig = plt.figure()
+    plt.plot(xdata, ydata, 'o', label=label1)
+    plt.plot(xdata, second_pass(xdata, *popt2), '-', label=label2)
+    plt.ylabel('Emission Intensity (a.u.)')
+    plt.xlabel('Photon Energy (eV)')
+    plt.yscale('log')
+    plt.title(plot_title)
+    plt.legend()
+    plt.show()
+    return p0_b, p0_c, pfinal_a, pfinal_b, pfinal_c, fig
+
+# Beginning Script 
+# looping = True
+# while looping: 
+for files in data_list: 
+    if (files == ".DS_Store"): 
+        pass 
+    else: 
+        #print(f'Currently Processing: {files}')
+        
+        # Reading .csv files 
+        test_df = pd.read_csv(path+f"/{files}")
+        col = test_df.columns
+        if (len(col) != 4):
+            print(f'{files} does not have the correct columns')
+            print(col)
+        else: 
+            components = files.split("_")
+            volts.append(components[1]) # Append input voltage
+            latticeT = int(components[0]) # Reading from long name 
+            bandE = 0.42+0.625*comp*(5.8/(latticeT+300)-4.19/(latticeT+271))*10**-4 * latticeT**2 * comp - (4.19*10**-4 * latticeT**2)/(latticeT+271) + 0.475*comp**2 # in eV, bandgap energy for InGaAs. 
+            # Creation of dataframe
+            
+                # out: Index(['Wavelength', 'S2c', 'Wavelength.1', 'S2'], dtype='object')
+            print(len(col))
+            #if (len(col) != 4):
+            #     if col
+            #     print("The .csv file has incorrect columns")
+            #     break
+            # else: 
+            #     pass 
+            # # Creating new df to not mess with original 
+            # new_df = pd.DataFrame() 
+            # new_df[col[0]] = test_df[col[0]].iloc[1:] # Wavelength, using iloc to remove non numerical value 
+            # new_df[col[1]] = test_df[col[1]].iloc[1:] # S2c 
+            # new_df[col[3]] = test_df[col[3]].iloc[1:] # S2
+            # new_df = new_df.astype(float) # Converting data values to numeric from strings, prep for parse in plot
+
+            # # Creating additional columns to be used for further calculations 
+            # new_df['Photon Energy'] = (h*c)/(new_df[col[0]]*10**-9)
+            # new_df['Multiplier'] = new_df['S2c']/new_df['S2']
+
+            # # dLambda conversion to dE 
+            # scale = 1240
+            # new_df['dE_Conv S2c'] =  new_df[col[1]] * new_df[col[0]] / scale
+            # new_df['dE_Conv S2'] = new_df[col[3]] * new_df[col[0]] / scale
+
+
